69f2a80309d9093ef386be947b7fe51d59e04e9f
DesktopDellMarcelo <marcelojpr96@gmail.com>
2018-03-15 15:07:03 +0000

    Exercicios complicados, má explicaçao da parte da equipa do xamarin.

---
+++ Exercicio 3/Start/MyTunes.NetStandard/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System.Net.Http;
+
+namespace MyTunes
+{
+    public static class SongExtensions
+    {
+        static HttpClient httpClient = new HttpClient();
+        public static string RuinSongName(this string songName)
+        {
+            return songName.Replace("Crocodile", "Alligator");
+        }
+    }
+}
